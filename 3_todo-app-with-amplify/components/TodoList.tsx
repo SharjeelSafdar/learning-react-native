618a3bb63ecc84720cf2ae487968f2bd20ac50f0
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-10-21 01:24:24 +0500

    Added a graphql api

--- 3_todo-app-with-amplify/components/TodoList.tsx
+++ 3_todo-app-with-amplify/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import {
   Text,
   HStack,
@@ -9,65 +9,181 @@ import {
   Input,
   VStack,
   View,
+  Spinner,
 } from "native-base";
 import { FontAwesome, Feather } from "@expo/vector-icons";
+import { API, graphqlOperation } from "aws-amplify";
+import { listTodos } from "../src/graphql/queries";
+import { createTodo, updateTodo, deleteTodo } from "../src/graphql/mutations";
+import {
+  CreateTodoMutationVariables,
+  CreateTodoMutation,
+  UpdateTodoMutationVariables,
+  UpdateTodoMutation,
+  DeleteTodoMutationVariables,
+  DeleteTodoMutation,
+} from "../src/API";
 
 const TodosList: FC = () => {
   const [todoText, setTodoText] = useState("");
-  const [currentId, setCurrentId] = useState(123456);
+  const [updatedTodoText, setUpdatedTodoText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [isEditing, setIsEditing] = useState<Todo | null>(null);
 
   interface Todo {
     id: string;
     text: string;
     completed: boolean;
+    createdAt: string;
+    updatedAt: string;
   }
-  const initialTodos: Todo[] = [
-    { id: "1", text: "Buy grocery.", completed: false },
-    { id: "2", text: "Wash the car.", completed: true },
-    { id: "3", text: "Play badminton.", completed: false },
-    { id: "4", text: "Make a todo app.", completed: true },
-  ];
-  const [todos, setTodos] = useState(initialTodos);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodo = () => {
-    setTodos(prev => [
-      { id: currentId.toString(), text: todoText, completed: false },
-      ...prev,
-    ]);
-    setCurrentId(prev => prev + 1);
-    setTodoText("");
-  };
-
-  const toggleTodo = (id: string) => {
-    setTodos(prev => {
-      return prev.map(todo =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+  const fetchTodos = async () => {
+    try {
+      setLoading(true);
+      const response = (await API.graphql(graphqlOperation(listTodos))) as any;
+      const allTodos = response.data.listTodos.items as Todo[];
+      console.log("AllTodos:", allTodos);
+      allTodos.sort(
+        (a, b) =>
+          new Date(a.updatedAt).getMilliseconds() -
+          new Date(b.updatedAt).getMilliseconds()
       );
-    });
-  };
-
-  const editTodo = (id: string) => {
-    const todoIndex = todos.findIndex(todo => todo.id === id);
-    if (todoIndex === -1) {
-      console.error("Edit Todo: invalid id");
+      setTodos(allTodos);
+    } catch {
+      console.error("Error fetching todos.");
+    } finally {
+      setLoading(false);
     }
-    const todoToEdit = todos[todoIndex];
-    deleteTodo(id);
-    setTodoText(todoToEdit.text);
   };
 
-  const deleteTodo = (id: string) => {
-    setTodos(prev => prev.filter(todo => todo.id !== id));
+  const addTodo = async () => {
+    try {
+      setLoading(true);
+      const variables: CreateTodoMutationVariables = {
+        input: {
+          text: todoText,
+          completed: false,
+        },
+      };
+      const response = (await API.graphql({
+        query: createTodo,
+        variables,
+      })) as { data: CreateTodoMutation };
+      const newTodo = response.data.createTodo;
+      console.log("NewTodo:", newTodo);
+      newTodo && setTodos(prev => [{ ...newTodo }, ...prev]);
+      setTodoText("");
+    } catch {
+      console.error("Error creating new todo.");
+    } finally {
+      setLoading(false);
+    }
   };
+
+  const toggleTodo = async (oldTodo: Todo) => {
+    try {
+      setLoading(true);
+      const variables: UpdateTodoMutationVariables = {
+        input: {
+          id: oldTodo.id,
+          completed: !oldTodo.completed,
+        },
+      };
+      const response = (await API.graphql({
+        query: updateTodo,
+        variables,
+      })) as { data: UpdateTodoMutation };
+      const updatedTodo = response.data.updateTodo;
+      console.log("ToggledTodo:", updatedTodo);
+      updatedTodo &&
+        setTodos(prev =>
+          prev.map(todo => (todo.id === oldTodo.id ? { ...updatedTodo } : todo))
+        );
+    } catch {
+      console.error("Error toggling todo!");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const startEditing = (todo: Todo) => {
+    setUpdatedTodoText(todo.text);
+    setIsEditing(todo);
+  };
+
+  const cancelEditing = () => {
+    setIsEditing(null);
+    setUpdatedTodoText("");
+  };
+
+  const editTodo = async () => {
+    try {
+      setLoading(true);
+      if (isEditing === null) {
+        throw new Error("Edit Todo: not in edit mode");
+      }
+      const variables: UpdateTodoMutationVariables = {
+        input: {
+          id: isEditing.id,
+          text: updatedTodoText,
+        },
+      };
+      const response = (await API.graphql({
+        query: updateTodo,
+        variables,
+      })) as { data: UpdateTodoMutation };
+      const updatedTodo = response.data.updateTodo;
+      console.log("UpdatedTodo:", updatedTodo);
+      updatedTodo &&
+        setTodos(prev =>
+          prev.map(todo =>
+            todo.id === isEditing.id ? { ...updatedTodo } : todo
+          )
+        );
+    } catch {
+      console.error("Error updating todo text!");
+    } finally {
+      setLoading(false);
+      setIsEditing(null);
+      setUpdatedTodoText("");
+    }
+  };
+
+  const deleteTodoFunc = async (id: string) => {
+    try {
+      setLoading(true);
+      const variables: DeleteTodoMutationVariables = {
+        input: { id },
+      };
+      const response = (await API.graphql({
+        query: deleteTodo,
+        variables,
+      })) as { data: DeleteTodoMutation };
+      const deletedTodo = response.data.deleteTodo;
+      console.log("DeletedTodo:", deletedTodo);
+      deletedTodo && setTodos(prev => prev.filter(todo => todo.id !== id));
+    } catch {
+      console.error("Error updating todo text!");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
   return (
     <VStack paddingX={5} paddingY={5} minW="100%">
       <HStack justifyContent="space-between" mb={5} space={3}>
         <Input
           value={todoText}
-          onChangeText={v => setTodoText(v)}
+          onChangeText={setTodoText}
           placeholder="Add Todo"
           flex={1}
+          isDisabled={!!isEditing}
         />
         <IconButton
           borderRadius="sm"
@@ -76,8 +192,39 @@ const TodosList: FC = () => {
           alignItems="center"
           icon={<Icon as={Feather} name="plus" size="sm" color="warmGray.50" />}
           onPress={addTodo}
+          disabled={!!isEditing}
         />
       </HStack>
+      <HStack justifyContent="space-between" mb={5} space={3}>
+        <Input
+          value={updatedTodoText}
+          onChangeText={setUpdatedTodoText}
+          placeholder="Updated Todo Text"
+          flex={1}
+          isDisabled={!isEditing}
+        />
+        <IconButton
+          borderRadius="sm"
+          variant="solid"
+          justifyContent="center"
+          alignItems="center"
+          icon={<Icon as={Feather} name="x" size="sm" color="warmGray.50" />}
+          onPress={cancelEditing}
+          disabled={!isEditing}
+        />
+        <IconButton
+          borderRadius="sm"
+          variant="solid"
+          justifyContent="center"
+          alignItems="center"
+          icon={
+            <Icon as={Feather} name="check" size="sm" color="warmGray.50" />
+          }
+          onPress={editTodo}
+          disabled={!isEditing}
+        />
+      </HStack>
+      {(loading || !!isEditing) && <Spinner size="lg" />}
       <FlatList
         data={todos}
         keyExtractor={(todo: Todo) => todo.id}
@@ -87,7 +234,8 @@ const TodosList: FC = () => {
               <Checkbox
                 isChecked={todo.completed}
                 value={todo.text}
-                onChange={() => toggleTodo(todo.id)}
+                onChange={() => toggleTodo(todo)}
+                isDisabled={!!isEditing}
               >
                 <Text mx={3} strikeThrough={todo.completed}>
                   {todo.text}
@@ -95,10 +243,11 @@ const TodosList: FC = () => {
               </Checkbox>
             </View>
             <IconButton
-              onPress={() => editTodo(todo.id)}
+              onPress={() => startEditing(todo)}
               size="sm"
               colorScheme="trueGray"
               px={3}
+              disabled={!!isEditing}
               icon={
                 <Icon
                   as={FontAwesome}
@@ -109,9 +258,10 @@ const TodosList: FC = () => {
               }
             />
             <IconButton
-              onPress={() => deleteTodo(todo.id)}
+              onPress={() => deleteTodoFunc(todo.id)}
               size="sm"
               colorScheme="trueGray"
+              disabled={!!isEditing}
               icon={
                 <Icon
                   as={FontAwesome}
---
+++ 3_todo-app-with-amplify/src/graphql/subscriptions.ts
@@ -0,0 +1,37 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      id
+      text
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      id
+      text
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      id
+      text
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
